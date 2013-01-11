b35ea21dcc71751c02d7de48c1fed6c03d735d1d
Kevin Van Wilder <kevin@inuits.eu>
2013-01-11 14:30:35 +0100

    Updated sax parser (thx Bert!) and extra testcase.

--- mediamosa/response.py
+++ mediamosa/response.py
@@ -1,123 +1,106 @@
-
-import xml.sax
 import xml.sax.handler
 
 _formats = {
     "item_count": int,
     "item_count_total": int,
     "item_offset": int,
-    "output": lambda buffer: buffer.replace("\r\n", "\n")}
+    "request_query_count": int,
+    "request_process_time": float,
+}
+_dictionaries = {
+    "dublin_core": lambda handler:
+        _setDictionary(handler._dictionaries[-1], key="dublin_core"),
+    "header": lambda handler:
+        handler.headers,
+    "item": lambda handler:
+        _setDictionary(handler.items),
+    "mediafile": lambda handler:
+        _setDictionary(handler.items[-1]["mediafiles"]),
+    "metadata": lambda handler:
+        _setDictionary(handler._dictionaries[-1], key="metadata"),
+    "qualified_dublin_core": lambda handler:
+        _setDictionary(handler._dictionaries[-1], key="qualified_dublin_core"),
+    "still": lambda handler:
+        _setDictionary(handler.items[-1]["mediafiles"][-1]["stills"]),
+}
+_lists = {
+    "mediafiles": lambda handler:
+        handler.items[-1].setdefault(u"mediafiles", []),
+    "still": lambda handler:
+        handler.items[-1].setdefault("mediafiles", [{}])[-1].setdefault(u"stills", []),
+}
+_ignore = [
+    "items",
+    "mediafiles",
+    "response",
+]
+
+
+def _setDictionary(container, key=None):
+    """Set a dictionary.
+    """
+    dictionary = {}
+
+    if type(container) == dict:
+        container[key] = dictionary
+    elif type(container) == list:
+        container.append(dictionary)
+
+    return dictionary
 
 
 class MediaMosaResponseContentHandler(xml.sax.handler.ContentHandler):
-    """Content-handler for Mediamosa-responses.
+    """Content-handler for responses from a MediaMosa-server.
     """
 
     def __init__(self):
         """Constructor.
         """
-        self.container = None
-        self.element = None
-
         self.headers = {}
         self.items = []
 
-        self.buffer = None
+        self._elements = []
+        self._dictionaries = []
+        self._content = None
 
     def startElement(self, name, attributes):
         """Handle start-element.
         """
-        if name == "header":
-            self.container = "header"
+        if name in _lists:
+            _lists[name](self)
 
-        elif name == "item":
-            self.container = "item"
-            self.items.append({})
+        if name in _dictionaries:
+            dict_ = _dictionaries[name](self)
+            self._dictionaries.append(dict_)
 
-        elif name in ("dublin_core", "qualified_dublin_core"):
-            self.container = name
-            self.items[-1][name] = {}
-
-        elif name == "mediafiles":
-            pass
-
-        elif name == "mediafile":
-            if "mediafiles" not in self.items[-1]:
-                self.items[-1]["mediafiles"] = []
-
-            self.container = "mediafile"
-            self.items[-1]["mediafiles"].append({})
-
-        elif name == "metadata":
-            self.container = "metadata"
-            if "mediafiles" in self.items[-1]:
-                self.items[-1]["mediafiles"][-1]["metadata"] = {}
-            else:
-                self.items[-1]["metadata"] = {}
-
-        elif name == "still":
-            if "stills" not in self.items[-1]["mediafiles"][-1]:
-                self.items[-1]["mediafiles"][-1]["stills"] = []
-
-            self.container = "still"
-            self.items[-1]["mediafiles"][-1]["stills"].append({})
-
-        elif self.container:
-            self.element = name
-            self.buffer = ""
+        self._elements.append(name)
+        self._content = u""
 
     def endElement(self, name):
         """Handle end-element.
         """
-        if name in ("header", "item"):
-            self.container = None
+        try:
+            if name in _ignore:
+                pass
+            elif name in _dictionaries:
+                self._dictionaries.pop()
+            elif self._elements[-2] in _dictionaries:
+                format_ = _formats.get(name, None)
+                if format_:
+                    content = format_(self._content)
+                else:
+                    content = self._content
 
-        elif name in ("dublin_core", "qualified_dublin_core", "mediafile"):
-            self.container = "item"
-
-        elif name == "mediafiles":
+                self._dictionaries[-1][name] = content
+        except IndexError:
             pass
 
-        elif name == "metadata":
-            if "mediafiles" in self.items[-1]:
-                self.container = "mediafile"
-            else:
-                self.container = "item"
-
-        elif name == "still":
-            self.container = "mediafile"
-
-        else:
-            format = _formats.get(self.element)
-            if format:
-                self.buffer = format(self.buffer)
-
-            if self.container == "header":
-                self.headers[self.element] = self.buffer
-
-            elif self.container == "item":
-                self.items[-1][self.element] = self.buffer
-
-            elif self.container in ("dublin_core", "qualified_dublin_core"):
-                self.items[-1][self.container][self.element] = self.buffer
-
-            elif self.container == "mediafile":
-                self.items[-1]["mediafiles"][-1][self.element] = self.buffer
-
-            elif self.container == "metadata":
-                if "mediafiles" in self.items[-1]:
-                    self.items[-1]["mediafiles"][-1]["metadata"][self.element] = self.buffer
-                else:
-                    self.items[-1]["metadata"][self.element] = self.buffer
-
-            elif self.container == "still":
-                self.items[-1]["mediafiles"][-1]["stills"][-1][self.element] = self.buffer
-
-            self.element = None
-            self.buffer = None
+        self._elements.pop()
+        self._content = None
 
     def characters(self, content):
         """Handle characters.
         """
-        if self.element:
-            self.buffer += content
+        if self._elements:
+            if self._content is not None:
+                self._content += content
