e2edc215feca1a05e83313430a4a73bc7809ce75
Kevin Van Wilder <kevin@inuits.eu>
2013-01-18 15:47:41 +0100

    Preperations for first release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 long_desc = open('README.txt').read()
 
 setup(
-    name='MediaMosa',
+    name='mediamosa',
     version='0.0.1',
     author='UGent Portaal Team',
     author_email='portaal-tech@ugent.be',
@@ -12,5 +12,22 @@ setup(
     url='http://www.mediamosa.org',
     license='LICENSE.txt',
     description='API wrapper for MediaMosa.',
-    long_description=long_desc
+    long_description=long_desc,
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          'Topic :: Multimedia :: Video',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Utilities'
+          ],
 )
